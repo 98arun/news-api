b74f9461f2fc47dd507f137796569b6c743ab37b
98arun <82587103+98arun@users.noreply.github.com>
2021-10-23 23:47:41 +0530

    Convert Ajax xml request into fetch api

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ let apiKey = "758a44ea44b14baebcc191b524ae2e4b";
 let stateNews = document.getElementById("stateNews");
 
 // Create an ajax get request
-const xhr = new XMLHttpRequest();
+/*const xhr = new XMLHttpRequest();
 xhr.open(
   "GET",
   `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`,
@@ -52,19 +52,97 @@ xhr.onload = function () {
   }
 };
 
-xhr.send();
+xhr.send();*/
 
-// let a = `<div class="card">
-// <div class="card-header" id="heading${index}">
-//     <h2 class="mb-0">
-//     <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
-//         aria-expanded="false" aria-controls="collapse${index}">
-//        <b>Breaking News ${index+1}:</b> ${element["title"]}
-//     </button>
-//     </h2>
-// </div>
+// Convert ajax xml request into fetch api
 
-// <div id="collapse${index}" class="collapse show" aria-labelledby="heading${index}" data-parent="#newsAccordion">
-//     <div class="card-body"><strong>Published at: ${element["publishedAt"]}</strong><br> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
-// </div>
-// </div>`;
+// function getData() {
+//   url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
+
+//   fetch(url)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log("LN65", data);
+//       let articles = data.articles;
+//       console.log("LN67", articles);
+//       let newsHtml = "";
+//       articles.forEach(function (element, index) {
+//         // console.log(element, index);
+//         let news = `<div class="accordion-item">
+//                         <h2 class="accordion-header" id="heading${index}">
+//                           <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}"><b>Breaking News
+//                           ${index + 1}:</b> ${element["title"]}
+//                           </button>
+//                         </h2>
+//                         <div id="collapse${index}" class="accordion-collapse collapse " aria-labelledby="heading
+//                         ${index}" data-bs-parent="#stateNews">
+//                           <div class="accordion-body"><strong>Published at:
+//                           ${element["publishedAt"]}</strong><br>
+//                          <strong> Content:</strong> ${
+//                            element["content"]
+//                          }. <a href="
+//                           ${element["url"]}" target="_blank" >Read more here</a>
+//                           </div>
+//                         </div>
+//                       </div>`;
+
+//         newsHtml += news;
+//       });
+//       // console.log(newsHtml);
+//       stateNews.innerHTML = newsHtml;
+//     })
+
+//     .catch((err) => {
+//       console.log("Invalid Api");
+//     });
+// }
+// getData();
+
+// By using async/await function
+
+async function getData() {
+  const res = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`
+  );
+  const data = await res.json();
+  return data;
+}
+
+getData()
+  .then((data) => {
+    console.log("LN114", data);
+    let articles = data.articles;
+    console.log("LN116", articles);
+    let newsHtml = "";
+    articles.forEach(function (element, index) {
+      // console.log(element, index);
+      let news = `<div class="accordion-item">
+                          <h2 class="accordion-header" id="heading${index}">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}"><b>Breaking News
+                            ${index + 1}:</b> ${element["title"]}
+                            </button>
+                          </h2>
+                          <div id="collapse${index}" class="accordion-collapse collapse " aria-labelledby="heading
+                          ${index}" data-bs-parent="#stateNews">
+                            <div class="accordion-body"><strong>Published at:
+                            ${element["publishedAt"]}</strong><br>
+                           <strong> Content:</strong> ${
+                             element["content"]
+                           }. <a href="
+                            ${
+                              element["url"]
+                            }" target="_blank" >Read more here</a>
+                            </div>
+                          </div>
+                        </div>`;
+
+      newsHtml += news;
+    });
+    // console.log(newsHtml);
+    stateNews.innerHTML = newsHtml;
+  })
+  .catch((err) => {
+    console.log("Invalid Api");
+  });
